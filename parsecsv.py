86af7a44a7e051bb59883d2abdf8175ed56970d3
Gaurish Sharma <sharmagaurish123@gmail.com>
2022-09-10 21:37:56 +1000

    get input of waste composition

--- parsecsv.py
+++ parsecsv.py
@@ -2,8 +2,29 @@
 
 import csv
 
-csv_filename = 'DatasetOfLakes.csv'
-with open(csv_filename) as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        print(row)
+def waste_input():
+    waste = {
+        "temp": 0.0,
+        "DO": 0.0,
+        "pH": 0.0,   
+        "conductivity": 0,
+        "BOD": 0.0,
+        "nitrate_n_nitrite": 0.0,
+        "fecal_coliform": 0
+    }
+
+    waste["temp"] = float(input("Enter temperature: "))
+    waste["DO"] = float(input("Enter dissolved oxygen (mg/l): "))
+    waste["pH"] = float(input("Enter pH: "))
+    waste["conductivity"] = int(input("Enter conductivity (mhos/cm): "))
+    waste["BOD"] = float(input("Enter biochemical oxygen demand (mg/l): "))
+    waste["nitrate_n_nitrite"] = float(input("Enter nitrate-n and nitrite-n (mg/l): "))
+    waste["fecal_coliform"] = float(input("Enter fecal coliform (mpn/100ml): "))
+
+    return waste
+
+
+
+
+
+
