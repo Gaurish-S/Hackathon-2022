df49dfbfe51b3ff2ab1579e7be2dc82293a1e959
ZuiSave <xoe.save@gmail.com>
2022-09-10 22:11:06 +1000

    parsing csv file done, need to create list of dicts

--- parsecsv.py
+++ parsecsv.py
@@ -1,9 +1,15 @@
-# Assuming that csv file is present in another file named DatasetOfLakes.csv
-
+import os
 import csv
 
-csv_filename = 'DatasetOfLakes.csv'
-with open(csv_filename) as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        print(row)
+def parse_lake_data():
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'waterquality.csv')
+
+    with open(filename, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+
+        for row in reader:
+            print(row)
+
+if __name__ == "__main__":
+    parse_lake_data()
