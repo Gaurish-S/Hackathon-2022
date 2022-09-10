567a9498452b8ceb69fd781e20282ae076a68e7f
Evan <shizongyan2002@gmail.com>
2022-09-11 02:55:50 +1000

    watching valorant esports rn

--- frontend.py
+++ frontend.py
@@ -1,10 +1,9 @@
 import PySimpleGUI as sg
 from parsecsv import set_waste, show_waste
 
-# Input window
-input_size = (40, 1)
-
-input_column = [
+def make_input_window():
+    input_size = (40, 1)
+    layout = [
     [sg.Text("User Manual Inputs", font=("Helvetica", 30))],
     [sg.Text("Enter Average Temperature Value:", size=input_size), sg.InputText(key='temperature'), sg.Button('Edit')],
     [sg.Text("Enter Average pH Level:", size=input_size), sg.InputText(key='pH'), sg.Button('Edit')],
@@ -14,11 +13,8 @@ input_column = [
     [sg.Text("Enter Average Value of Nitrate-n and Nitrite-n:", size=input_size), sg.InputText(key='nitrate_n'), sg.Button('Edit')],
     [sg.Text("Enter Average Fecal Coliform:", size=input_size), sg.InputText(key='coliform'), sg.Button('Edit')],
     [sg.Button('Submit')],
-]
-
-input_layout = [input_column]
-
-input_window = sg.Window("Input Window", input_layout, element_justification='c')
+    ]
+    return sg.Window('Input Window', layout, element_justification='c', modal=True)
 
 temperature = 'Edit'
 pH = 'Edit0'
@@ -29,39 +25,41 @@ nitrate = 'Edit4'
 coliform = 'Edit5'
 submit = 'Submit'
 
-# Run the Event Loop
-while True:
-    event, values = input_window.read()
-    if event == "Exit" or event == sg.WIN_CLOSED:
-        input_window.close()
-        quit()
-    elif event == temperature:
-        set_waste("temp", values['temperature'])
-        show_waste("temp")
-    elif event == pH:
-        set_waste("pH", values['pH'])
-        show_waste("pH")
-    elif event == oxygen:
-        set_waste("DO", values['oxygen'])
-        show_waste("DO")
-    elif event == conductivity:
-        set_waste("conductivity", values['conductivity'])
-        show_waste("conductivity")
-    elif event == biochemical:
-        set_waste("BOD", values['biochemical'])
-        show_waste("BOD")
-    elif event == nitrate:
-        set_waste("nitrate_n_nitrite", values['nitrate_n'])
-        show_waste("nitrate_n_nitrite")
-    elif event == coliform:
-        set_waste("fecal_coliform", values['coliform'])
-        show_waste("fecal_coliform")
-    elif event == submit:
-        print("submitted")
-        break
+def input_window_function():
+    input_window = make_input_window()
 
-input_window.close()
+    while True:
+        event, values = input_window.read()
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            break
+        elif event == temperature:
+            set_waste("temp", values['temperature'])
+            show_waste("temp")
+        elif event == pH:
+            set_waste("pH", values['pH'])
+            show_waste("pH")
+        elif event == oxygen:
+            set_waste("DO", values['oxygen'])
+            show_waste("DO")
+        elif event == conductivity:
+            set_waste("conductivity", values['conductivity'])
+            show_waste("conductivity")
+        elif event == biochemical:
+            set_waste("BOD", values['biochemical'])
+            show_waste("BOD")
+        elif event == nitrate:
+            set_waste("nitrate_n_nitrite", values['nitrate_n'])
+            show_waste("nitrate_n_nitrite")
+        elif event == coliform:
+            set_waste("fecal_coliform", values['coliform'])
+            show_waste("fecal_coliform")
+        elif event == submit:
+            print("submitted")
+            break
+    input_window.close()
 
+# Input window
+input_window_function()
 
 # Data Window
 button_size = (30, 1)
@@ -74,11 +72,34 @@ data_column = [
      sg.Button("Sort by Average Conductivity Level", size=button_size), 
      sg.Button("Sort by Average Biochemical Oxygen Demand", size=button_size), 
      sg.Button("Sort by Average Nitrate-n and Nitrite-n Values", size=button_size), 
-     sg.Button("Sort by Average Fecal Coliform", size=button_size)]
+     sg.Button("Sort by Average Fecal Coliform", size=button_size)],
+     [sg.Button("Show Input Window")]
 ]
 
-data_layout = [data_column]
 
+
+# Get data from backend, update table
+def create_table():
+    table_layout = [
+        [
+            sg.Table(
+                headings=['River name:', 
+                'Temperature', 
+                'pH Level', 
+                'Dissolved Oxygen:', 
+                'Conductivity Level', 
+                'Biochemical Oxygen Demand', 
+                'Value of Nitrate-n and Nitrite-n', 
+                'Fecal Coliform'],
+                values=[['1', '2', '3', '4', '5', '6', '7', '8']],
+                justification='center',
+            )
+        ]
+    ]
+    return table_layout
+
+
+data_layout = [[data_column], [create_table()]]
 
 data_window = sg.Window("Zui\'s River Scouting Application", data_layout, element_justification='c')
 
@@ -86,5 +107,21 @@ while True:
     event, values = data_window.read()
     if event == "Exit" or event == sg.WIN_CLOSED:
         break
+    elif event == "Show Input Window":
+        input_window_function()
+    elif event == "Sort by Average Temperature":
+        print("change to temp")
+    elif event == "Sort by Average pH":
+        print("change to pH")
+    elif event == "Sort by Average Value of Dissolved Oxygen":
+        print("change to dissolved oxygen")
+    elif event == "Sort by Average Conductivity Level":
+        print("change to conductivity")
+    elif event == "Sort by Average Biochemical Oxygen Demand":
+        print("change to biochemical")
+    elif event == "Sort by Average Nitrate-n and Nitrite-n Values":
+        print("change to nitrate-n")
+    elif event == "Sort by Average Fecal Coliform":
+        print("change to coliform")
 
 data_window.close()
