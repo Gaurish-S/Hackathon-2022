428123a80dd58c73a119e01ea0202c5f58f1ca37
Gaurish Sharma <sharmagaurish123@gmail.com>
2022-09-11 12:46:29 +1000

    fixed order

--- convert.py
+++ convert.py
@@ -1,7 +1,16 @@
 def dictionary_to_list(list_of_dict):
     new_list = []
-    for key in list_of_dict:
-        new_list.append(key.values())
+    for item in list_of_dict:
+        new_list.append([
+            item["location"], 
+            item["temp"], 
+            item["pH"],
+            item["DO"],
+            item["conductivity"],
+            item["BOD"],
+            item["nitrate_n_nitrite"],
+            item["fecal_coliform"]
+        ])
     return new_list
 
 if __name__ == "__main__":
